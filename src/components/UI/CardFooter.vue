9f2fb2af44b49474740c67d6f532981a1a895393
Crusader99 <smoker68@mail.ru>
2024-11-15 14:40:18 +0300

    hw 2.1

---
+++ src/components/UI/CardFooter.vue
@@ -0,0 +1,23 @@
+<template >
+    <div  class="one-third">
+      
+        <span class="stat">{{ title }}</span>
+        <br/>
+        <span class="stat-value">{{ value}}</span>
+      
+    </div>
+  </template>
+  <script>
+    export default {
+      props: {
+          value:{
+              type :String,
+              requred: true
+          },
+          title:{
+              type :String,
+              requred: true
+          }
+        }
+    }
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="md-body">
-  </div>
-  <div class="card-wrapper" v-for="item in items" :key="item.id">
+  
+    <carousel  :itemsToShow="1"  wrapArround="true"  snapAlign="center">
+  
+  
+  <slide class="card-wrapper" v-for="item in items" :key="item.id">
     <Card :title="item.title" 
       :name="item.lvl + ' уровень'" 
       :descr="item.descr" 
@@ -13,28 +15,43 @@
       <template v-slot:footer>
         <div class="card-stats">
           <div v-for="(stat,index) in item.info" :key="index" class="one-third">
-            <span class="stat">{{stat.title}}</span>
-            <br/>
-            <span class="stat-value">{{stat.value}}</span>
+            
+            <CardFooterVue :title="stat.title" :value="stat.value">
+              
+            </CardFooterVue>
           </div>
         </div>
       </template>
       
     </Card>
-  </div>
+  </slide>
+  <template #addons>
+    <navigation />
+  </template>
+
+</carousel>
+ 
   
 </template>
 
 <script>
-  import items from "@/seeders/items.js"
-  import Card from "@/components/UI/Card.vue"
+  import 'vue3-carousel/dist/carousel.css';
+  import { Carousel, Slide,  Navigation } from 'vue3-carousel';
+  import items from "@/seeders/items.js";
+  import Card from "@/components/UI/Card.vue";
+  import CardFooterVue from '@/components/UI/CardFooter.vue';
   export default {
-    components: {Card},
+    components: {
+      Card,
+      Carousel,
+      Slide,
+      CardFooterVue,
+      Navigation
+    },
     data() {
       return {
         items:items,
       }
-
     }
   }
 </script>
